cefa36171bd62363912728fe09c04d5ea793b32f
Soham Kudtarkar <skudtarkar@berkeley.edu>
2018-02-13 18:55:07 -0800

    Minor fix

--- schedules/schedule_translator.py
+++ schedules/schedule_translator.py
@@ -4,7 +4,7 @@ import sys
 import csv
 
 times = ",9:00 AM,9:30 AM,10:00 AM,10:30 AM,11:00 AM,11:30 AM,12:00 PM,12:30 PM,1:00 PM,1:30 PM,2:00 PM,2:30 PM,3:00 PM,3:30 PM,4:00 PM,4:30 PM,5:00 PM,5:30 PM,6:00 PM,6:30 PM,7:00 PM,7:30 PM,8:00 PM,8:30 PM,9:00 PM"
-days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 with open("Auto-ELF Availabilities (Responses) - Form Responses 1.csv", newline='') as csvfile:
 	filereader = csv.reader(csvfile, skipinitialspace=True)
